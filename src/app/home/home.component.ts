7fc5873a67d62f9c3e276adfad43ee812a681471
Ben <ben.ng@outlook.com>
2019-06-05 14:10:54 -0400

    finished typeahead functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
+  gameList = [
+    "League of Legends",
+    "Overwatch",
+    "DOTA 2"
+  ];
+  filteredGameList: any[];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  filterGameList(event) {
+    this.filteredGameList = [];
+
+    this.gameList.forEach((game) => {
+      if(game.toLowerCase().indexOf(event.query.toLowerCase()) == 0)
+        this.filteredGameList.push(game);
+    });
+
+    if(this.filteredGameList.length == 0)
+      this.filteredGameList.push("We\'re not playing " + event.query + " yet. Click here to request to play it!");
+  }
+
+  selectGame(game) {
+    console.log(game + " selected");
   }
 
 }
